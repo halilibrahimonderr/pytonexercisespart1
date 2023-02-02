a1140b65d70735a3991d592f0d0d2bac0f04553c
halilibrahimonderr <halil9460@gmail.com>
2023-02-02 23:35:26 +0300

    exercises11

---
+++ exercises11.py
@@ -0,0 +1,7 @@
+# Modify the variables as suggested
+a = 'hello' #capitalize
+b = 'tom' #uppercase
+c = 'LAURA' #lowercase
+question = 'How are you' #change o in e
+age_question = 'How old are you?' #use the correct method to create a string for each word
+print(a.capitalize(), b.upper(), c.lower(), question.replace('o','e'), age_question.split())
