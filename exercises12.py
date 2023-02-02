527aecb30fa17b01ea29fcef8f5ecdce5857382f
halilibrahimonderr <halil9460@gmail.com>
2023-02-02 23:57:35 +0300

    exercises12

---
+++ exercises12.py
@@ -0,0 +1,6 @@
+# Create two variables called name and age. Then create a variable called 'hello' and print the name and the age.
+# Use the formatted string literal. 
+name='Adam'
+age=33
+hello="hello, {}.Are you {}".format(name,age)
+print(hello)
