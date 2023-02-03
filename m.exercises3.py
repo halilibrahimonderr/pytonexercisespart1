1cbaa42e0896081d1ee2773fab13569de2d132f6
halilibrahimonderr <halil9460@gmail.com>
2023-02-03 11:15:37 +0300

    exercisemethods3

--- m.exercises3.py
+++ m.exercises3.py
@@ -10,8 +10,9 @@ print(num1_str[2:5]) # get the 3-5 elements of string (both inclusive) by string
 print(num2 in num1_str) # check if num2 in string (cast if necessary)
 print(num3 in num1_str) # check if num3 in string (cast if necessary)
 
+
 string_with_0='0'+num1_str
 print(string_with_0) # concatenate 0 to the string from left and assign to string_with_0
 print(string_with_0[:4]) # get the characters of string_with_0 from start to position 4 (end point exclusive)
 print(string_with_0[5:]) # get the characters of string_with_0 from position 5 until the end
-print(string_with_0[-10:-7]) # use negative indexing to reach the "567" string_with_0
+print(string_with_0[-10:-7]) # use negative indexing to reach the "567" string_with_0 
