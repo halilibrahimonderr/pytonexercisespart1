c0de6e4957e615f1745ed1728335acccffc11cfe
halilibrahimonderr <halil9460@gmail.com>
2023-02-03 09:27:46 +0300

    exercisemethods1

---
+++ m.exercises1.py
@@ -0,0 +1,12 @@
+#With type command you can see the type of a variable in python 
+# eg: type(1) print each components type with this way you can see most significant data types in python
+print(type('selam'))
+print(type([1,2,3,'selam']))
+print(type(True))
+print(type({'honda':'civic','volvo':'s90'}))
+print(type(('osman','orhan','mustafa',1961,1964,1969)))
+print(type(63))
+print(type(40.9))
+print(type(20 + 5j))
+print(type(['zeynep','ayse','rabia']))
+print(type({'zeynep','ayse','rabia'}))
