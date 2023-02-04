0981f1f39929677754566ff16f7cc59d63b39333
halilibrahimonderr <halil9460@gmail.com>
2023-02-04 19:22:05 +0300

    exercises conditions1

---
+++ c.exercises1.py
@@ -0,0 +1,19 @@
+# print if num1 or num2 is greater num1 = 335 num2 = 66
+# where num1 is greater than num2
+num1 = 335
+num2 = 66
+
+if num1 > num2 :
+    print(f'{num1} is greater than {num2}')
+else:
+    print(f'{num1} is not greater than {num2}')
+
+
+# where num2 is  greater than num1
+num1=49
+num2=66
+
+if num2>num1 :
+    print (f'{num2} is greater than {num1}')
+else:
+    print(f'{num2} is not greater than {num1}')
